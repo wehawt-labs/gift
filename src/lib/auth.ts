2fca5d82cac21e3a56d1cec12fe7b9769b7eea05
thanghq99 <haikhauak@gmail.com>
2026-02-13 00:21:23 +0700

    feat: temporary disable better auth database adapter

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -4,15 +4,15 @@ import { db } from '@/db'
 import * as schema from '@/db/schema'
 
 export const auth = betterAuth({
-  database: drizzleAdapter(db, {
-    provider: 'pg',
-    schema: {
-      ...schema
-      // Mapping the "users" table to the "user" table in Drizzle if needed
-      // But we can just pass the schema and BetterAuth will find its tables
-    },
-    usePlural: true
-  }),
+  // database: drizzleAdapter(db, {
+  //   provider: 'pg',
+  //   schema: {
+  //     ...schema
+  //     // Mapping the "users" table to the "user" table in Drizzle if needed
+  //     // But we can just pass the schema and BetterAuth will find its tables
+  //   },
+  //   usePlural: true
+  // }),
   emailAndPassword: {
     enabled: true
   },
